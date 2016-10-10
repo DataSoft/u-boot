684297a8fdfbd5878a249b88ef3d421c604972bb
Aaron Brice <aaron.brice@datasoft.com>
2016-10-10 12:30:01 -0700

    Adding 1279 Sidebridge Iridium board

---
+++ include/configs/dsvsom-1279.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2016 DataSoft Corp
+ *
+ * Configuration settings for the DataSoft Universal Sidebridge
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "dsvsom-common.h"
+
+#endif
